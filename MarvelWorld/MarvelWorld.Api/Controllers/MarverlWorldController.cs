edd436dfb48b2c7c9e254017b529c1fbb3b50725
Marco Hung <mhungc@gmail.com>
2020-04-28 18:04:37 +0200

    Initial Api for Marvel World

---
+++ MarvelWorld/MarvelWorld.Api/Controllers/MarverlWorldController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MarvelWorld.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarverlWorldController : ControllerBase
+    {
+        private readonly ILogger<MarverlWorldController> _logger;
+
+        public MarverlWorldController(ILogger<MarverlWorldController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: api/MarverlWorld
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Spider Man", "X-Men" };
+        }
+
+        // GET: api/MarverlWorld/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/MarverlWorld
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/MarverlWorld/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
